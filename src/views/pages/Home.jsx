57154023406504b396a388a992751559e5b75c7f
Chidiebere95 <chidiezeokwelume@gmail.com>
2023-10-02 17:12:08 +0100

    add github pages

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -19,6 +19,48 @@ function Home() {
   }, []);
   return (
     <div className='App'>
+      <table>
+        <thead>
+          <tr>
+            <th>column 1</th>
+            <th>column 2</th>
+            <th>column 3</th>
+            <th>column 4</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>item1</td>
+            <td>item2</td>
+            <td>item3</td>
+            <td>item4</td>
+          </tr>
+        </tbody>
+      </table>
+      {lorem.status === 'loading' ? (
+        <p>Loading...</p>
+      ) : lorem.status === 'success' ? (
+        <div>
+          {lorem.data.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+          <div
+            onClick={() => {
+              navigator.clipboard.writeText(lorem.data.join(''));
+            }}
+            className='copy'
+          >
+            <button>Copy all</button>
+            <figure className={`${theme}_theme_color`}>{icons.copy}</figure>
+          </div>
+        </div>
+      ) : lorem.status === 'error' || lorem.status === 'base' ? (
+        <p>
+          Unable to connect to the internet. check your network and try again...
+        </p>
+      ) : (
+        <></>
+      )}
       {lorem.status === 'loading' ? (
         <p>Loading...</p>
       ) : lorem.status === 'success' ? (
